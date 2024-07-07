427c993967cb8581cc2ec99e0321d76cb2d76a64
Xavier Oillic <xavieroillic@gmail.com>
2024-07-08 01:00:56 +0200

    pick a card if 0 and not pick if not 0

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -10,31 +10,39 @@ import { CardService } from '../card.service';
 export class HomePageComponent implements OnInit {
   playButton: string = 'CLICK TO PLAY DISPLAY!';
 
-  countdown: number = 60; // Set the initial countdown value (in seconds)
-  interval: any;
-
   constructor(private cardService: CardService) {}
 
   cardToDiplay!: Card;
 
+  //******* */ COUNTDOWN FROM 60 SECONDS TO 0. ***********
+
+  countdown: number = 15; // Set the initial countdown value (in seconds)
+  interval: any;
   startCountdown() {
     this.interval = setInterval(() => {
       if (this.countdown > 0) {
         this.countdown--;
       } else {
         clearInterval(this.interval);
-        alert('Countdown is over!');
+        //alert('Countdown is over, you can pick one card!');
       }
     }, 1000); // Update the countdown every 1 second (1000 ms)
   }
 
   // méthode du bouton : affiche et store in localStorage.
   displayTheCard() {
-    this.cardService.getOneCard().subscribe((dataCard) => {
-      console.log("Affichage d'une carte => ", dataCard);
-      this.cardToDiplay = dataCard;
-      this.cardService.setCardStringify('picked up Card', dataCard);
-    });
+    if (this.countdown > 0) {
+      this.cardToDiplay == null;
+    } else if (this.countdown == 0) {
+      this.cardService.getOneCard().subscribe((dataCard) => {
+        console.log("Affichage d'une carte => ", dataCard);
+        this.cardToDiplay = dataCard;
+        this.cardService.setCardStringify('picked up Card', dataCard);
+      });
+    } else {
+      clearInterval(this.interval);
+      this.startCountdown();
+    }
   }
 
   ngOnInit(): void {
@@ -49,5 +57,18 @@ displayTheCard() {
       this.cardToDiplay = dataCard;
       this.cardService.setCardStringify('picked up Card', dataCard);
     });
+  }
+
+  displayTheCard() {
+    if (this.countdown > 0) {
+      this.cardToDiplay == null;
+    } else {
+      this.cardService.getOneCard().subscribe((dataCard) => {
+        console.log("Affichage d'une carte => ", dataCard);
+        this.cardToDiplay = dataCard;
+        this.cardService.setCardStringify('picked up Card', dataCard);
+        this.startCountdown();
+      });
+    }
   }
 */
