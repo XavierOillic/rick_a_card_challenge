95db4b9c6b5b4c31c58e17697a31c01ace23a6dd
Xavier Oillic <xavieroillic@gmail.com>
2024-07-12 20:15:03 +0200

    fonctionnel

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -8,17 +8,71 @@ import { CardService } from '../card.service';
   styleUrls: ['./home-page.component.css'],
 })
 export class HomePageComponent implements OnInit {
-  playButton: string = 'CLICK TO PLAY : DISPLAY A CARD!';
-  buttonDisabled: Boolean = false;
-
   constructor(private cardService: CardService) {}
 
   cardToDiplay!: Card;
-
-  //******* */ COUNTDOWN FROM 60 SECONDS TO 0. ***********
+  playButton: string = 'CLICK TO PLAY : DISPLAY A CARD!';
+  buttonDisabled: Boolean = false;
 
   countdown: number = 10; // Set the initial countdown value (in seconds)
-  interval: any;
+  //interval: any;
+
+  //************************************************************************* */
+
+  ngOnInit(): void {}
+
+  //*********** SERVICE METHOD: display and store in localStorage the card ***********
+  serviceDisplay() {
+    this.cardService.getOneCard().subscribe((dataCard) => {
+      console.log("Affichage d'une carte => ", dataCard);
+      this.cardToDiplay = dataCard;
+      this.cardService.setCardStringify('picked up Card', dataCard);
+    });
+  }
+
+  //********************* START COUNTDOWN ******************************************** */
+
+  startCountdown() {
+    const interval = setInterval(() => {
+      if (this.countdown > 0) {
+        this.countdown--;
+      } else {
+        clearInterval(interval);
+        console.log('quesquispass ');
+      }
+    }, 1000);
+    this.countdown = 10;
+  }
+
+  //********************* START TIME STOP ******************************************** */
+
+  timing() {
+    this.buttonDisabled = true;
+    setTimeout(() => {
+      this.buttonDisabled = false;
+    }, 10000);
+  }
+
+  //***********************  BUTTON ENABLED EACH 10 SECONDS ****************************************** */
+
+  displayTheCard() {
+    this.serviceDisplay();
+    this.startCountdown();
+    this.timing();
+  }
+}
+
+//************************** EN DESSOUS BROUILLON *************************************** */
+/*
+
+setTimeout(() => {
+  console.log("Delayed for 10 second.");
+  // le code ici est lancé 10 secondes après.
+}, "10000");
+
+
+
+
   startCountdown() {
     this.interval = setInterval(() => {
       if (this.countdown > 0) {
@@ -28,50 +82,5 @@ export class HomePageComponent implements OnInit {
         //alert('Countdown is over, you can pick one card!');
       }
     }, 1000); // Update the countdown every 1 second (1000 ms)
-  }
-
-  // méthode du bouton : affiche et store in localStorage.
-  displayTheCard() {
-    if (this.countdown > 0) {
-      this.buttonDisabled == true;
-    }
-    if (this.countdown == 0) {
-      this.cardService.getOneCard().subscribe((dataCard) => {
-        console.log("Affichage d'une carte => ", dataCard);
-        this.cardToDiplay = dataCard;
-        this.cardService.setCardStringify('picked up Card', dataCard);
-      });
-    } else if (this.cardToDiplay) {
-      this.buttonDisabled == true && this.startCountdown();
-    }
-  }
-
-  ngOnInit(): void {
-    if (this.cardToDiplay) {
-      this.startCountdown();
-    }
-  }
-}
-
-/*
-displayTheCard() {
-    this.cardService.getOneCard().subscribe((dataCard) => {
-      console.log("Affichage d'une carte => ", dataCard);
-      this.cardToDiplay = dataCard;
-      this.cardService.setCardStringify('picked up Card', dataCard);
-    });
-  }
-
-  displayTheCard() {
-    if (this.countdown > 0) {
-      this.cardToDiplay == null;
-    } else {
-      this.cardService.getOneCard().subscribe((dataCard) => {
-        console.log("Affichage d'une carte => ", dataCard);
-        this.cardToDiplay = dataCard;
-        this.cardService.setCardStringify('picked up Card', dataCard);
-        this.startCountdown();
-      });
-    }
   }
 */
